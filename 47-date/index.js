ec1c7a0959105d9930129d9e7030f79890e28921
annguy <ngqanlc1995@gmail.com>
2020-04-05 18:40:49 +0700

    update code

--- 47-date/index.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment');
-
-function isWeekend(dateString) {
-    // Write code here...
-    let date = moment(dateString);
-    return date.isoWeekday() === 6 || date.isoWeekday() === 7;
-}
-
-isWeekend('2020-04-03');
